c315c5bd116128ae645eb3ee8b2b6aea2e615ab6
jireve999 <wbajxw999@aliyun.com>
2025-05-16 22:13:38 +0800

    UI setup

---
+++ src/components/FilterMain.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="filter-main">
+    <div class="filter-main-left"></div>
+    <div class="filter-main-right"></div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.filter-main {
+  flex: 1;
+  display: flex;
+  .filter-main-left {
+    width: 97px;
+    background-color: #323947;
+  }
+  .filter-main-right {
+    flex: 1;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { MapManager } from '../js/map-manager';
 import { onMounted } from 'vue';
+import FilterHeader from '../components/FilterHeader.vue';
+import LocationBtn from '../components/LocationBtn.vue';
+import SelectArea from '../components/SelectedArea.vue';
+import FilterMain from '../components/FilterMain.vue';
 
 onMounted(() => {
   console.log('HomeView mounted');
@@ -44,7 +48,20 @@ function init() {
 <template>
   <div class="home-view">
     <div class="map-layer" id="map"></div>
-    <!-- <div class="ui-layer"></div> -->
+    <div class="ui-layer">
+      <div class="filter-container">
+        <div class="filter-content">
+          <div class="close-btn">
+            <div class="close-icon"></div>
+          </div>
+          <LocationBtn />
+          <SelectArea />
+          <FilterHeader />
+          <div class="search-container">搜索</div>
+          <FilterMain />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -59,6 +76,54 @@ function init() {
     top: 0;
     width: 100%;
     height: 100%;
+    z-index: 1;
+  }
+  .ui-layer {
+    position: absolute;
+    left: 0;
+    top: 0;
+    // width: 100%;
+    height: 100%;
+    z-index: 2;
+    .filter-container {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 415px;
+      height: 100%;
+      padding: 20px;
+      .filter-content {
+        display: flex;
+        flex-direction: column;
+        background-color: #3b4354;
+        width: 100%;
+        height: 100%;
+        border-radius: 12px;
+        .close-btn { 
+          position: absolute;
+          top: 32px;
+          right: -44px;
+          width: 64px;
+          height: 40px;
+          background-image: url('../assets/images/ui/close-bg.png');
+          background-size: cover;
+          display: flex;
+          align-items: center;
+          box-sizing: border-box;
+          padding-left: 18px;
+          .close-icon {
+            width: 24px;
+            height: 24px;
+            background-image: url('../assets/images/ui/close-icon.png');
+            background-size: cover;
+          }
+        }
+        .search-container {
+          width: 100%;
+          height: 52px;
+        }
+      }
+    }
   }
 }
 </style>
