3244841af2d70f5b6a08555c23b53a57f02d0ad5
jireve999 <wbajxw999@aliyun.com>
2025-05-17 23:13:33 +0800

    location shown

--- src/components/LocationBtn.vue
+++ src/components/LocationBtn.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="location-btn"></div>
+  <div class="location-btn">
+    <div class="location-icon"></div>
+    <div class="location-content">
+      <div class="location-title">快速定位</div>
+      <div class="content-area">
+        <div class="area-item" v-for="item in 10" :key="item">
+          <div class="area-parent">
+            <div class="parent-icon"></div>
+            <div class="parent-name">来歆山</div>
+          </div>
+          <div class="area-child" v-for="item in 5" :key="item">赤望台</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
@@ -14,5 +28,79 @@
   background-color: rgba(50,57,71,.8);
   border-radius: 8px;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    .location-content {
+      visibility: visible;
+    }
+  }
+  .location-content {
+    position: absolute;
+    left: 60px;
+    top: 0;
+    background: #3b4354;
+    border-radius: 12px;
+    visibility: hidden;
+    transition: all .3s;
+    .location-title {
+      font-size: 16px;
+      padding: 12px;
+      color: #d3bc8e;
+      border-bottom: 1px solid rgba(86,97,120,0.2);
+      width: 192px;
+    }
+   .content-area {
+      height: 500px;
+      overflow-y: auto;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+     .area-item {
+        .area-child { 
+          display: flex;
+          align-items: center;
+          padding-left: 32px;
+          height: 48px;
+          color: hsla(39, 34%, 89%, 0.75);
+          &:hover {
+            color: #ece5d8;
+          }
+        }
+        .area-parent {
+          display: flex;
+          align-items: center;
+          padding-left: 15px;
+          height: 48px;
+          &:hover {
+            .parent-name {
+              color: #ece5d8;
+            }
+            .parent-icon {
+              background-image: url("../assets/images/ui/location-icon-h.png");
+            }
+          }
+          .parent-icon {
+            width: 12px;
+            height: 12px;
+            background-image: url("../assets/images/ui/location-icon-n.png");
+            background-size: cover;
+            margin-right: 5px;
+          }
+          .parent-name {
+            color: #d3bc8e;
+            font-size: 14px;
+          }
+        }
+     }
+   }
+  }
+  .location-icon {
+    width: 24px;
+    height: 24px;
+    background: url("../assets/images/ui/location-btn.png");
+    background-size: cover;
+  }
 }
 </style>
